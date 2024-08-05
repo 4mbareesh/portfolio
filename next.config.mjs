68d51c7037679d3ad6eebdd2b09ef5a503228d23
4mbareesh <57589799+4mbareesh@users.noreply.github.com>
2024-08-05 22:25:30 +0530

    chore: update config files

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+import withBundleAnalyzer from '@next/bundle-analyzer'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig)
