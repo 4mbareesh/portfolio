44015ebb3ff1aab5bbebdc08b3243ff7df331409
4mbareesh <57589799+4mbareesh@users.noreply.github.com>
2024-11-09 22:54:24 +0530

    fix: refactor logic to handle exceptions

--- src/components/home/hero-bg.tsx
+++ src/components/home/hero-bg.tsx
@@ -1,19 +1,17 @@
 'use client'
 
-import React from 'react'
 import { ShootingStars } from '../ui/shooting-stars'
 import { StarsBackground } from '../ui/stars-background'
 import { useTheme } from 'next-themes'
 
 function HeroBg() {
   const { resolvedTheme } = useTheme()
-  if (resolvedTheme === 'dark')
-    return (
-      <>
-        <ShootingStars className="fixed -z-10" />
-        <StarsBackground className="fixed -z-20" />
-      </>
-    )
+  return resolvedTheme === 'dark' ? (
+    <>
+      <ShootingStars className="fixed -z-10" />
+      <StarsBackground className="fixed -z-20" />
+    </>
+  ) : null
 }
 
 export default HeroBg
