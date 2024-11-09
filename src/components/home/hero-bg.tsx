027e3cc6b19eb08a4636e5ef56e4220be00c3e04
4mbareesh <57589799+4mbareesh@users.noreply.github.com>
2024-11-10 00:20:51 +0530

    feat: add seperate bg components for desktop and smaller screens

--- src/components/home/hero-bg.tsx
+++ src/components/home/hero-bg.tsx
@@ -1,17 +1,39 @@
 'use client'
 
+import { useWindowSize } from '@/hooks/use-window-size'
+import Particles from '../ui/particles'
 import { ShootingStars } from '../ui/shooting-stars'
 import { StarsBackground } from '../ui/stars-background'
 import { useTheme } from 'next-themes'
+import { memo } from 'react'
 
 function HeroBg() {
   const { resolvedTheme } = useTheme()
-  return resolvedTheme === 'dark' ? (
+  const { isBreakpoint } = useWindowSize()
+  const isDark = resolvedTheme === 'dark'
+  const isLargeScreen = isBreakpoint('lg')
+
+  if (!isDark) return null
+
+  return (
     <>
-      <ShootingStars className="fixed -z-10" />
-      <StarsBackground className="fixed -z-20" />
+      {isLargeScreen ? (
+        <Particles
+          className="fixed inset-0 -z-50 duration-500 animate-in fade-in"
+          aria-hidden="true"
+        />
+      ) : (
+        <StarsBackground
+          className="fixed -z-20 duration-500 animate-in fade-in"
+          aria-hidden="true"
+        />
+      )}
+      <ShootingStars
+        className="fixed -z-10 duration-500 animate-in fade-in"
+        aria-hidden="true"
+      />
     </>
-  ) : null
+  )
 }
 
-export default HeroBg
+export default memo(HeroBg)
