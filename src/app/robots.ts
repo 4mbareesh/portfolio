8e1ac484b8419957fe7d9a7f5123c5866b75c620
4mbareesh <57589799+4mbareesh@users.noreply.github.com>
2025-01-10 23:30:45 +0530

    fix: robots sitemap url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: process.env.CLIENT_URL,
+    sitemap: process.env.CLIENT_URL + '/sitemap.xml',
   }
 }
